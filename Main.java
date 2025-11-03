6743432c68b7003a38a04ca69c7fb55ef00fdbf2
Kamran Manzoor <kamranmanzoor1448@gmail.com>
2025-11-03 11:57:27 +0500

    fisrt commit on java

---
+++ Main.java
@@ -0,0 +1,47 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.println(12 + 32);
+    System.out.println(12 - 2);
+    // use for comment in java programing.
+    System.out.println("i am java developer"); //this code about java developer.
+    /* i am learning java programing language. Jav is popular langauge.
+    and im very glad to learn java. thank you.*/
+    // creating variable in java as syntax: data_type variable_name = value;
+    String name = "java bro";
+    System.out.println(name);
+    int number = 34;
+    System.out.println(number);
+    float decimalNumber = 3.14f;
+    System.out.println(decimalNumber);
+    char letter = 'A';
+    System.out.println(letter);
+    boolean isJavaFun = true;
+    System.out.println(isJavaFun);
+
+
+    final int num = 122; // constant variable
+    System.out.println(num);
+
+    final float num1 = 112.33f;
+    System.out.println(num1);
+
+    String fname = "java";
+    String lname = "developer";
+    System.out.println(fname + " " + lname);
+
+    int x = 9;
+    int y = 11;
+    System.out.println(x + y);
+
+    // multiple variable in one line1
+    int x1 = 12, y1 = 33, z1 = 45;
+    System.out.println("sum is : " + (x1 + y1 + z1));
+
+    // one value to multiple variable
+    int a, b, c;
+    a = b = c = 50;
+    System.out.println(a + b + c);
+
+}
+}
