8bf0dca29c359ea767dae64add1710015a81841f
Kamran Manzoor <kamranmanzoor1448@gmail.com>
2025-11-12 00:57:22 +0500

    java

---
+++ Demo.java
@@ -0,0 +1,28 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hellow world!");
+
+        System.out.println("------------------------");
+        // create vaiable
+        String name = "John-bro";
+        int num1 = 23;
+         System.out.println(name);
+         System.out.println(num1);
+
+        //  final variable
+        final float pai  = 3.1416f;
+        final int MINUTES_PER_HOUR = 60;
+        final int BIRTHYEAR = 1980;
+        final double PI = 3.14159;
+        System.out.println(PI);
+        System.out.println(pai);
+        System.out.println(MINUTES_PER_HOUR);
+        System.out.println(BIRTHYEAR);
+
+        // multiple variable
+
+        int x, y, z;
+        x = y = z = 23;
+        System.out.println(x + y + z);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # java-basic-with-DSA
 Here i will share notes and codes about java basics and data structure and algorithms.
+
+W3School:
+--> java is a popular programming language use for develop apps website and games etc.
+--> it is created  in 1995.
+-->first code of java:
+    ``` public class Main () {
+           public static void main (String [], args) {
+            System.out.println ("Hellp World!");
+           }
+    }```
+
+--> It is owned by Oracle, and more than 3 billion devices run Java.
+
+--> creating variable : String myName = "python!"
+--> multiple vairable : int x, y, z; , x = 23 = y = z = 11;
